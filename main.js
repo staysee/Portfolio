d35a37f2a5dc2316918187f535a6414c92935dce
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-05-26 00:28:06 -0700

    working on greybox

---
+++ main.js
@@ -0,0 +1,7 @@
+const toggleCollapse = document.querySelector('.toggle-collapse span');
+const nav = document.querySelector('.nav')
+
+// onclick event on toggle Collapse span tag
+toggleCollapse.onclick = () => {
+    nav.classList.toggle('collapse')
+}
