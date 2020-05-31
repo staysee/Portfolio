3fa7abbf45a5f82fa303b0cfcc8197e0702699e1
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-05-30 22:20:10 -0700

    redone mobile first

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ const nav = document.querySelector('.nav')
 // onclick event on toggle Collapse span tag
 toggleCollapse.onclick = () => {
     nav.classList.toggle('collapse')
+    console.log('clicked')
 }
