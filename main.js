057e7b5922b613328f6534dc0602b1f879e69ef1
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-06-07 00:30:36 -0700

    fixed navbar styling

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-$('.nav .toggle-collapse span').click( () => {
-    $('.nav').toggleClass('collapse')
-})
+function mobileNav() {
+    if ($('.nav').css('display') === "block") {
 
-$('.nav-items a').click( () => {
-    $('.nav').toggleClass('collapse')
-    $('.nav').addClass('quick-nav')
-    console.log('close')
-})
+        $('.nav-items a').click( () => {
+            $('.nav').toggleClass('collapse')
+            $('.nav').addClass('quick-nav')
+            console.log('close')
+        })
+
+        $('.nav .toggle-collapse span').click( () => {
+            $('.nav').toggleClass('collapse')
+        })
+    }
+}
+
+$(mobileNav)
